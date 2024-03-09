5ae42ed0f1e2f95da1afe63b15b9e34eb0efddd9
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-09 19:21:24 +0200

    Update Skills

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,10 +1,64 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 
 const Backend = () => {
   return (
-    <div>Backend</div>
-  )
-}
+    <div className="skills__content">
+      <h3 className="skills__title">Backend developer</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Node.js, Express.js</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
 
-export default Backend
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Restful API, JWT</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Moongose, MVC</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MongoDB, MySQL</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Postman, MongoDb Compass</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name"></h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -1,10 +1,72 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 
 const Frontend = () => {
   return (
-    <div>Frontend</div>
-  )
-}
+    <div className="skills__content">
+      <h3 className="skills__title">Frontend developer</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">HTML5, CSS3</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
 
-export default Frontend
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Bootstrap5, Tailwind</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Javascript, Typescript</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Typescript</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">React.js, Redux</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Next.js</h3>
+              <span className="skills__level">Advanced</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Remix.js, ReactNative</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frontend;
