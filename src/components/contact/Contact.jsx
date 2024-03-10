a4bff772b4c3b089daed270d44f7edea9813cd39
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-10 04:25:41 +0200

    Update Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,64 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
+import { Link } from "react-router-dom";
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="contact section" id="contact">
+      <h2 className="section__title">Get in touch</h2>
+      <span className="section__subtitle">Contact Me</span>
 
-export default Contact
+      <div className="contact__container container grid">
+        <div className="contact__content">
+          <h3 className="contact__title">Talk to me</h3>
+          <div className="contact__info">
+            <div className="contact__card">
+              <i className="bx bx-mail-send contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Email</h3>
+              <span className="contact__card-data">user@gmail.com</span>
+
+              <a
+                href="mailto:alaa.hamdy.mohammadi@gmail.com.com"
+                className="contact__button"
+              >
+                Write me{" "}
+                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+
+            <div className="contact__card">
+              <i className="bx bxl-whatsapp contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Whatsapp</h3>
+              <span className="contact__card-data">999-888-777</span>
+
+              <Link to="" className="contact__button">
+                Write me{" "}
+                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </Link>
+            </div>
+
+            <div className="contact__card">
+              <i className="bx bxl-messenger contact__card-icon"></i>
+
+              <h3 className="contact__card-title"></h3>
+              <span className="contact__card-data"></span>
+
+              <Link to="" className="contact__button">
+                Write me{" "}
+                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="contact__content">
+          <h3 className="contact__title">Write me your project</h3>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
