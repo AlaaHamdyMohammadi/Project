e8743a6885cfaac34bb783ddab575b00df4ee82c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-13 01:37:39 +0200

    Update Work

---
+++ src/components/work/WorkItems.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const WorkItems = ({ item }) => {
+  return <div className="work__card">{item}</div>;
+};
+
+export default WorkItems
