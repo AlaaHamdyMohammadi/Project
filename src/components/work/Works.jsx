f86c3ea97a4310f5851efcc09338ecf994daca62
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-14 02:24:24 +0200

    Update Work

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -4,12 +4,35 @@ import { projectsData, projectsNav } from "./Data";
 import WorkItems from "./WorkItems";
 
 const Works = () => {
+  const [item, setItem] = React.useState({ name: "all" });
+  const [projects, setProjects] = React.useState([]);
+  const [active, setActive] = React.useState(0);
+
+  React.useEffect(() => {
+    if (item.name === "all") {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category === item.name;
+      });
+      setProjects(newProjects);
+    }
+  }, [item]);
+
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent });
+  };
+
   return (
     <div>
       <div className="work__filters">
         {projectsNav.map((item, index) => {
           return (
-            <span className="work__item" key={index}>
+            <span
+              onClick={(e) => {handleClick(e, index)}}
+              className="work__item"
+              key={index}
+            >
               {item.name}
             </span>
           );
@@ -17,7 +40,7 @@ const Works = () => {
       </div>
 
       <div className="work__container container grid">
-        {projectsData.map((item) => {
+        {projects.map((item) => {
           return <WorkItems item={item} key={item.id} />;
         })}
       </div>
