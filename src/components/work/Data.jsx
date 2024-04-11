d6485db7593ca6f1ac13dab9a1bacebd9973d80d
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-04-11 18:06:11 +0200

    Add links

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -3,20 +3,23 @@ import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
 import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
+import Bankist from "../../assets/Bankist.jpg";
+import Noon from "../../assets/Noon.jpeg";
 
 export const projectsData = [
   {
     id: 1,
-    image: Work1,
+    image: Noon,
     title: "Noon website",
     category: "JavaScript",
     link: "https://noon-website-nu.vercel.app/",
   },
   {
     id: 2,
-    image: Work2,
-    title: "App movil",
-    category: "app",
+    image: Bankist,
+    title: "Bankist",
+    category: "JavaScript",
+    link: "https://bankist-project-psi.vercel.app/",
   },
   {
     id: 3,
@@ -33,8 +36,9 @@ export const projectsData = [
   {
     id: 5,
     image: Work5,
-    title: "Web design",
-    category: "web",
+    title: "Noon website",
+    category: "JavaScript",
+    link: "https://noon-website-nu.vercel.app/",
   },
 ];
 
